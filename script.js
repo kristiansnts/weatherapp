6b880d07314addd80a1350b6cb141ab3078fd406
kristiansnts <epafroditus.kritian@gmail.com>
2023-11-10 12:08:23 +0700

    search connect with weather api

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const YOUR_API_KEY = '92678efb0ed448d291755159230911';
+const searchButtons = document.querySelectorAll('.search-button');
+
+searchButtons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        event.preventDefault();
+        const inputValue = event.target.previousSibling.previousSibling.value;
+        weatherApi(inputValue);
+    })
+})
+
 
 async function weatherApi(place) {
     try {
         let response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${YOUR_API_KEY}&q=${place}&aqi=No`);
         let result = await response.json();
-        console.log(result.current);
+        console.log(result);
     } catch (error) {
         console.error(error);
     }
 }
-
-
-weatherApi('Geneng');
